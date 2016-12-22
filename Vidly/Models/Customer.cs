dad11f666ce6ad14934a737efff671262b17d1c8
issam boumlic <iboumlic@gmail.com>
2016-12-22 00:07:48 -0800

    connect to database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,12 @@ namespace Vidly.Models
     public class Customer
     {
         public int customerID { get; set; }
+        [Required]
+        [StringLength(255)]
         public string customerName { get; set; }
         public string customerEmail { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public int MembershipTypeID { get; set; }
     }
 }
